c554ab7e6109ffe023410ed1402e1c1790e41fd5
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2020-05-30 17:26:55 +0900

    多言語サポートのテスト

--- src/main/ArcaeaCalculationTool.java
+++ src/main/ArcaeaCalculationTool.java
@@ -1,5 +1,19 @@
 package main;
 
+import java.util.Locale;
+
 public class ArcaeaCalculationTool {
+
+	public static void main(String args[]) {
+		Locale defaultLocale = Locale.getDefault();
+
+		System.out.println(Messages.MSGSwitchToChartConstant);
+
+		Locale.setDefault(Locale.US);
+
+		System.out.println(Messages.MSGSwitchToChartConstant);
+
+		Locale.setDefault(defaultLocale);
+	}
 
 }
---
+++ src/main/Messages.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * プログラム内で使用されるメッセージを一元管理するクラス。
+ * メッセージ内容はsrc/languagesの中にあるプロパティファイルにあります。
+ *
+ * @author hizumiaoba
+ */
+
+	public enum Messages{
+		MSGSwitchToPotential,
+		MSGSwitchToStep,
+		MSGSwitchToExp,
+		MSGSwitchToPerformance,
+		MSGSwitchToChartConstant;
+
+	@Override public String toString() {
+		try {
+			return ResourceBundle.getBundle("languages.", Locale.getDefault()).getString(name());
+		}catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
