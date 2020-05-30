e2903121da432df9c644e55a41694778f6032f44
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2020-05-30 23:07:11 +0900

    譜面別ポテンシャル計算用クラス作成、計算に必要な譜面定数を纏めたデータベースJSONを、MemoryArchive楽曲のみ作成完了

---
+++ src/calcLib/ChartPotential.java
@@ -0,0 +1,14 @@
+package calcLib;
+
+/**
+ * その名の通り譜面別ポテンシャル珪砂尿のライブラリ
+ * ライブラリっつてもここで計算処理が行われます
+ * @author hizumi
+ *
+ */
+
+public class ChartPotential {
+
+	private static final double MAXCORRECTION = 2.0;
+
+}
