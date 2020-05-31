b2c7a88f9655599731e13749c7a980a378fda086
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2020-05-31 09:57:50 +0900

    譜面別ポテンシャルライブラリのコメント誤字修正、AbsoluteReason、EternalCore、SunsetRadiance、ViciousLabyrinthパックのライブラリ収録

--- src/calcLib/ChartPotential.java
+++ src/calcLib/ChartPotential.java
@@ -1,7 +1,7 @@
 package calcLib;
 
 /**
- * その名の通り譜面別ポテンシャル珪砂尿のライブラリ
+ * その名の通り譜面別ポテンシャル計算用のライブラリ
  * ライブラリっつてもここで計算処理が行われます
  * @author hizumi
  *
@@ -9,6 +9,7 @@ package calcLib;
 
 public class ChartPotential {
 
+	// 譜面定数へ加算される最大の補正値です。PMをとると、譜面定数＋この値で固定となります。
 	private static final double MAXCORRECTION = 2.0;
 
 }
