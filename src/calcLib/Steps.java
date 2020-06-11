427a33fd1b4593fecc8ee1d346db18d7b56e3595
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2020-06-11 09:21:51 +0900

    Update Steps.java
    コメントアウトにて改善策を記述

--- src/calcLib/Steps.java
+++ src/calcLib/Steps.java
@@ -7,6 +7,16 @@ public class Steps {
 		res = 2.5 + (2.45 * Math.sqrt(ChartPotential.calcChartPotential(pack, title, difficulty, score)));
 		return res;
 	}
-
-
+	/*
+	
+	public static double calcSteps(String pack, String title, String difficulty, int score) {
+		return calcSteps(pack, title, difficulty, score, 50);
+	}
+	
+	public static double calcSteps(String, pack, String title, String difficulty, int score, double, partner) {
+		double res = 0.00;
+			res = 2.5 + (2.45 * Math.sqrt(ChartPotential.calcChartPotential(pack, title, difficulty, score)));
+			return res * (partner * 0.02);
+	}
+	*/
 }
